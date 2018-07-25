e7bfac716cc185f50a4cabfafbe2cc50b07346ab
cynthiaokello <cynthiaokello050@gmail.com>
2018-07-25 08:30:54 +0300

    made changes to the file

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+ $("p").hide();
+ $("#btn").click(function(){
+     if ($("p").is(":hidden")==true){
+         $("p").show();
+         $("#btn").val('Purchase');
+     }
+     else{
+       $("p").hide();
+       $("#btn").val('show');
+     }
+ });
+});
