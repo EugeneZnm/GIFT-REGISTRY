2b2d167a99049884d42fac6b01056b1063950116
EugeneZnm <eugenenzioki@gmail.com>
2018-07-27 11:52:43 +0300

    checkout

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,21 @@
+$(document).ready(function() {
+    $("p").hide();
+    $("#btn").click(function(event) {
+        event.preventDefault();
+        if ($("p").is(":hidden") == true) {
+            $("p").show();
+            $("#btn").val("Purchase");
+        } else {
+            $("p").hide();
+            $("#btn").val("Purchase");
+        };
+        if ($("container-2").is(show()) == true) {
+            $("container-2").hide();
+            $("#btn").val("Purchase");
 
-$(document).ready(function(){
- $("p").hide();
- $("#btn").click(function(event){
-   event.preventDefault();
-     if ($("p").is(":hidden")==true){
-         $("p").show();
-         $("#btn").val("Purchase");
-     }
-     else{
-       $("p").hide();
-       $("#btn").val("Purchase");
-     };
- });
+        } else {
+            $("container-2").show();
+            $("#btn").val("Purchase");
+        }
+    });
 });
